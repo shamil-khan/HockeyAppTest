d6cc0d0bac3ea910aef7db562b3806d5e8fce853
shamil-khan <shamil.ahmed@gmail.com>
2019-08-07 17:04:01 +0500

    init from VS template

---
+++ HockeyAppTest/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using HockeyAppTest.Services;
+using HockeyAppTest.Views;
+
+namespace HockeyAppTest
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ HockeyAppTest/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using HockeyAppTest.Models;
+
+namespace HockeyAppTest.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
